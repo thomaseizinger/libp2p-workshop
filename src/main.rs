496648816cf337abe49677437488b42b7338c9f0
Thomas Eizinger <thomas@eizinger.io>
2023-04-14 18:19:51 +0200

    Remove clap

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use clap::Parser;
-use std::error::Error;
-
-#[derive(Debug, Parser)]
-#[clap(name = "libp2p-workshop-node")]
-struct Opts {}
-
 #[async_std::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    let _opts = Opts::parse();
 
     println!("Hello, world!");
 
