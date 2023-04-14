b01a87bf21437b7f336e9efb9f59ea09bc82fbf6
Thomas Eizinger <thomas@eizinger.io>
2023-04-15 00:05:58 +0200

    Add gossipsub chat

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -3,9 +3,17 @@ use futures::channel::mpsc;
 use futures::future::Either;
 use futures::StreamExt;
 use libp2p::swarm::SwarmEvent;
-use libp2p::{ping, Multiaddr, Swarm};
+use libp2p::{gossipsub, ping, Multiaddr, Swarm};
+
+const CHAT_TOPIC: &'static str = "workshop-chat";
 
 pub async fn run(mut swarm: Swarm<Behaviour>, mut commands: mpsc::Receiver<Command>) {
+    swarm
+        .behaviour_mut()
+        .gossipsub
+        .subscribe(&gossipsub::IdentTopic::new(CHAT_TOPIC))
+        .unwrap();
+
     loop {
         match futures::future::select(swarm.select_next_some(), commands.next()).await {
             Either::Left((SwarmEvent::ConnectionEstablished { endpoint, .. }, _)) => {
@@ -33,9 +41,33 @@ pub async fn run(mut swarm: Swarm<Behaviour>, mut commands: mpsc::Receiver<Comma
             )) => {
                 log::info!("RTT tp {peer} is {}ms", rtt.as_millis());
             }
+            Either::Left((
+                SwarmEvent::Behaviour(BehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    message,
+                    ..
+                })),
+                _,
+            )) => {
+                let source = message
+                    .source
+                    .map(|p| p.to_string())
+                    .unwrap_or("unknown".to_owned());
+
+                log::info!(
+                    "Message from {}: {}",
+                    source,
+                    String::from_utf8_lossy(&message.data)
+                );
+            }
             Either::Left((_, _)) => {}
             Either::Right((Some(Command::Dial(addr)), _)) => {
                 let _ = swarm.dial(addr);
+            }
+            Either::Right((Some(Command::Publish(message)), _)) => {
+                let _ = swarm
+                    .behaviour_mut()
+                    .gossipsub
+                    .publish(gossipsub::IdentTopic::new(CHAT_TOPIC), message.into_bytes());
             }
             Either::Right((None, _)) => {
                 log::info!("Sender dropped, terminating event loop");
@@ -46,5 +78,6 @@ pub async fn run(mut swarm: Swarm<Behaviour>, mut commands: mpsc::Receiver<Comma
 }
 
 pub enum Command {
+    Publish(String),
     Dial(Multiaddr),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod event_loop;
 
 use crate::event_loop::Command;
+use async_std::io;
+use async_std::io::prelude::BufReadExt;
+use async_std::io::BufReader;
 use env_logger::Env;
 use futures::channel::mpsc;
-use futures::SinkExt;
+use futures::{SinkExt, TryStreamExt};
 use libp2p::swarm::keep_alive;
 use libp2p::{
-    core::upgrade::Version, dns, identify, identity, noise, ping, swarm::SwarmBuilder, tcp, yamux,
-    Transport,
+    core::upgrade::Version, dns, gossipsub, identify, identity, noise, ping, swarm::SwarmBuilder,
+    tcp, yamux, Transport,
 };
 
 const BOOTSTRAP_NODE: &str = "/dns4/libp2p-workshop-bootnode.fly.dev/tcp/9999";
@@ -28,12 +31,9 @@ async fn main() -> anyhow::Result<()> {
         .multiplex(yamux::YamuxConfig::default())
         .boxed();
 
-    let swarm = SwarmBuilder::with_async_std_executor(
-        transport,
-        Behaviour::new(local_key.public()),
-        local_peer_id,
-    )
-    .build();
+    let swarm =
+        SwarmBuilder::with_async_std_executor(transport, Behaviour::new(local_key), local_peer_id)
+            .build();
 
     let (mut sender, receiver) = mpsc::channel(1);
 
@@ -41,7 +41,30 @@ async fn main() -> anyhow::Result<()> {
 
     sender.send(Command::Dial(BOOTSTRAP_NODE.parse()?)).await?;
 
-    futures::future::pending::<()>().await;
+    let mut reader = BufReader::new(io::stdin()).lines();
+
+    while let Some(line) = reader.try_next().await? {
+        match line
+            .split(":")
+            .map(|s| s.trim())
+            .collect::<Vec<&str>>()
+            .as_slice()
+        {
+            ["send", message] => {
+                sender.send(Command::Publish((*message).to_owned())).await?;
+            }
+            ["dial", address] => {
+                sender.send(Command::Dial(address.parse()?)).await?;
+            }
+            ["quit"] => {
+                break;
+            }
+            [other, ..] => {
+                log::warn!("Unknown command: {other}");
+            }
+            [] => {}
+        }
+    }
 
     Ok(())
 }
@@ -51,17 +74,23 @@ pub struct Behaviour {
     ping: ping::Behaviour,
     keep_alive: keep_alive::Behaviour,
     identify: identify::Behaviour,
+    gossipsub: gossipsub::Behaviour,
 }
 
 impl Behaviour {
-    fn new(local_key: identity::PublicKey) -> Self {
+    fn new(local_key: identity::Keypair) -> Self {
         Self {
             ping: ping::Behaviour::default(),
             keep_alive: keep_alive::Behaviour,
             identify: identify::Behaviour::new(identify::Config::new(
                 "/libp2p-workshop/1.0.0".into(),
-                local_key,
+                local_key.public(),
             )),
+            gossipsub: gossipsub::Behaviour::new(
+                gossipsub::MessageAuthenticity::Signed(local_key),
+                gossipsub::Config::default(),
+            )
+            .unwrap(),
         }
     }
 }
